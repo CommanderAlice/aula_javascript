2aa7ca0162bfb59b78b8163b02613ab020e69433
CommanderAlice <contatocaique02@gmail.com>
2021-08-05 01:16:44 -0300

    javascript

---
+++ JS/main.js
@@ -0,0 +1,103 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>You little shit child</b>";
+    //alert("Downloading boobs again Joel?!")
+}
+
+function redirecionar(){
+    window.open("https://fitgirlrepacks.co/");
+    window.location.href = "https://fitgirlrepacks.co/";
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "JOEL!!!!";
+    //document.getElementById("mousemove").innerHTML = "JOEL!!!!";
+    //alert("Trocar texto")
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui"
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Pagina carregada")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+
+
+
+/*
+function soma(n1, n2){
+    return n1 + n2
+}
+alert(soma(5, 10));
+*/
+
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade >=18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade");
+console.log(validaIdade(idade));
+*/
+
+
+/*
+var d = new Date();
+alert(d.getMinutes());
+alert(d.getMonth()+1);
+*/
+/*
+var count;
+for(count=0; count <=5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count =0;
+while (count <= 5){
+    console.log(count);
+    count++;
+}
+*/
+/*
+var idade = prompt("Qual sua idade");
+if (idade >=18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(fruta.nome)
+alert(fruta.cor)
+*/
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome)
+alert(fruta.cor)
+*/
+
+
+//var lista = ("maça", "pêra", "laranja");
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));}
